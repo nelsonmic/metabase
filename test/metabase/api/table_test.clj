090a034072abe9107ceb0653cae4afc438b2bffb
Cam Saul <github@camsaul.com>
2022-08-12 15:26:12 -0700

    Optimize fingerprint lookup in test

--- test/metabase/api/table_test.clj
+++ test/metabase/api/table_test.clj
@@ -491,36 +491,38 @@
                   :dimension_options (default-dimension-options)
                   :fields            (map (comp #(merge (default-card-field-for-venues card-virtual-table-id) %)
                                                 with-field-literal-id)
-                                          [{:name           "NAME"
-                                            :display_name   "NAME"
-                                            :base_type      "type/Text"
-                                            :effective_type "type/Text"
-                                            :semantic_type  "type/Name"
-                                            :fingerprint    (:fingerprint (mt/col :venues :name))
-                                            :field_ref      ["field" "NAME" {:base-type "type/Text"}]}
-                                           {:name           "ID"
-                                            :display_name   "ID"
-                                            :base_type      "type/BigInteger"
-                                            :effective_type "type/BigInteger"
-                                            :semantic_type  nil
-                                            :fingerprint    (:fingerprint (mt/col :venues :id))
-                                            :field_ref      ["field" "ID" {:base-type "type/BigInteger"}]}
-                                           (with-numeric-dimension-options
-                                             {:name           "PRICE"
-                                              :display_name   "PRICE"
-                                              :base_type      "type/Integer"
-                                              :effective_type "type/Integer"
+                                          (let [id->fingerprint   (db/select-id->field :fingerprint Field :table_id (mt/id :venues))
+                                                name->fingerprint (comp id->fingerprint (partial mt/id :venues))]
+                                            [{:name           "NAME"
+                                              :display_name   "NAME"
+                                              :base_type      "type/Text"
+                                              :effective_type "type/Text"
+                                              :semantic_type  "type/Name"
+                                              :fingerprint    (name->fingerprint :name)
+                                              :field_ref      ["field" "NAME" {:base-type "type/Text"}]}
+                                             {:name           "ID"
+                                              :display_name   "ID"
+                                              :base_type      "type/BigInteger"
+                                              :effective_type "type/BigInteger"
                                               :semantic_type  nil
-                                              :fingerprint    (:fingerprint (mt/col :venues :price))
-                                              :field_ref      ["field" "PRICE" {:base-type "type/Integer"}]})
-                                           (with-coordinate-dimension-options
-                                             {:name           "LATITUDE"
-                                              :display_name   "LATITUDE"
-                                              :base_type      "type/Float"
-                                              :effective_type "type/Float"
-                                              :semantic_type  "type/Latitude"
-                                              :fingerprint    (:fingerprint (mt/col :venues :latitude))
-                                              :field_ref      ["field" "LATITUDE" {:base-type "type/Float"}]})])})
+                                              :fingerprint    (name->fingerprint :id)
+                                              :field_ref      ["field" "ID" {:base-type "type/BigInteger"}]}
+                                             (with-numeric-dimension-options
+                                               {:name           "PRICE"
+                                                :display_name   "PRICE"
+                                                :base_type      "type/Integer"
+                                                :effective_type "type/Integer"
+                                                :semantic_type  nil
+                                                :fingerprint    (name->fingerprint :price)
+                                                :field_ref      ["field" "PRICE" {:base-type "type/Integer"}]})
+                                             (with-coordinate-dimension-options
+                                               {:name           "LATITUDE"
+                                                :display_name   "LATITUDE"
+                                                :base_type      "type/Float"
+                                                :effective_type "type/Float"
+                                                :semantic_type  "type/Latitude"
+                                                :fingerprint    (name->fingerprint :latitude)
+                                                :field_ref      ["field" "LATITUDE" {:base-type "type/Float"}]})]))})
                (->> card
                     u/the-id
                     (format "table/card__%d/query_metadata")
